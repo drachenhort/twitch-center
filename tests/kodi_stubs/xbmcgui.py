559ab296e100f359e31f9912c5bf15180709e939
Stephan <stephan.schiemann@gmail.com>
2026-08-11 16:19:56 +0200

    test: extend xbmcgui stub with controls and action constants

--- tests/kodi_stubs/xbmcgui.py
+++ tests/kodi_stubs/xbmcgui.py
@@ -1,16 +1,44 @@
 """Minimal stand-in for Kodi's built-in xbmcgui module, for pytest-only use."""
 
+ACTION_PREVIOUS_MENU = 10
+ACTION_NAV_BACK = 92
+
+
+class Action:
+    def __init__(self, action_id):
+        self._action_id = action_id
+
+    def getId(self):
+        return self._action_id
+
+
+class ControlLabel:
+    def __init__(self):
+        self._label = ""
+
+    def setLabel(self, text):
+        self._label = text
+
+    def getLabel(self):
+        return self._label
+
 
 class WindowXML:
     def __init__(self, xml_filename, script_path, default_skin="Default", default_res="1080i"):
         self.xml_filename = xml_filename
         self.script_path = script_path
+        self._controls = {}
 
     def show(self):
         pass
 
     def close(self):
         pass
+
+    def getControl(self, control_id):
+        if control_id not in self._controls:
+            self._controls[control_id] = ControlLabel()
+        return self._controls[control_id]
 
 
 class WindowXMLDialog(WindowXML):
--- tests/test_kodi_stubs.py
+++ tests/test_kodi_stubs.py
@@ -24,3 +24,27 @@ def test_xbmcaddon_stub_addon_getters():
     assert addon.getSetting("chat_display_mode") == ""
     assert addon.getSettingBool("does_not_exist") is False
     assert addon.getAddonInfo("id") == "script.twitch.center"
+
+
+def test_xbmcgui_stub_action_constants_and_getid():
+    import xbmcgui
+    assert xbmcgui.ACTION_PREVIOUS_MENU == 10
+    assert xbmcgui.ACTION_NAV_BACK == 92
+    action = xbmcgui.Action(92)
+    assert action.getId() == 92
+
+
+def test_xbmcgui_stub_control_label_set_and_get():
+    import xbmcgui
+    label = xbmcgui.ControlLabel()
+    label.setLabel("hello")
+    assert label.getLabel() == "hello"
+
+
+def test_xbmcgui_stub_windowxml_getcontrol_returns_label_and_persists():
+    import xbmcgui
+    win = xbmcgui.WindowXML("dummy.xml", "/tmp")
+    control = win.getControl(101)
+    control.setLabel("world")
+    assert win.getControl(101).getLabel() == "world"
+    assert win.getControl(102).getLabel() == ""
